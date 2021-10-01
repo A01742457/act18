28bb8e399c6cd1a67e43c822f94d6f88c9941d46
A01742457 <a01742457@tec.mx>
2021-10-01 14:16:24 +0000

    CuentaPares resuelto

---
+++ assignments/00HelloWorld/src/ej1.py
@@ -0,0 +1,25 @@
+def main():
+   n = int(input(">>>"))
+   m = int(input(">>>")
+   
+   
+    if n < 2:
+       print ("Error!")
+    elif m < 2:
+        print("Error!")
+    else:
+        matriz=[]
+        dato = 1
+
+        for row in range(n):
+            renglon=[]
+            for column in range(m):
+                renglon.append(dato)
+                dato= dato + 1
+            matriz.append(renglon)
+        print(matriz)
+
+        
+
+if __name__=='__main__':
+    main()
